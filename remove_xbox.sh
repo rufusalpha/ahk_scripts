d63034daeccb59200d27da91b2ee1273b8fc4eb6
Jakub Millan <jakubmillan@frozenway.games>
2023-08-08 17:42:35 +0200

    added generating the config file from script showing scripts and reading that config file from removing script

--- remove_xbox.sh
+++ remove_xbox.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
 #setup
-things=("FrozenDistrict.HouseFlipper_5.1.1.0_neutral__zytsvtd1jsvc4" 
-"FrozenDistrict.HouseFlipper-Pets_5.1.1.0_neutral__kqzqk4xtpwaqc" 
-"FrozenDistrict.HouseFlipper-Luxury_5.1.1.0_neutral__kqzqk4xtpwaqc" 
-"FrozenDistrict.HouseFlipper-Garden_5.1.1.0_neutral__kqzqk4xtpwaqc");
+address="";
+things="";
 
-address="10.10.10.160";
+#init
+if [ ! -f "output.cfg" ]; then
+    address="10.10.10.160";
+else
+    address=$(head -n 1 output.cfg);
+	flag=true;
+
+	while IFS= read -r line; do
+		if [ $flag == true ]; then
+			address=$line;
+			flag=false;
+		else
+			things+=($line);
+		fi 
+	done < output.cfg
+fi
+
+echo -n "Enter ip address (empty for default: $address): " 
+read x;
+
+if [[ $x != "" ]]; then
+    address=$x;
+fi
 
 #logic
 
-echo "Do you want to delete all builds? (y/n) ";
-read x
+echo "Do you want to delete all builds? (y/n)";
+read x;
 
 if [ $x == "y" ]; then
 	x="";
-	echo "Are you absolutely sure? (y/n) "
-	read x
+	echo "Are you absolutely sure? (y/n) ";
+	read x;
 	
 	if [ $x == "y" ]; then
 
 		for i in ${things[@]}; do
-			echo $i;
+			echo "   "$i;
 			xbapp -x $address uninstall $i;
 		done
 	fi
 fi
 
+#ending
 echo "Script done. . . Press enter to exit";
 read x;
 
--- show_builds_xbox.sh
+++ show_builds_xbox.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
-#setup
-address="10.10.10.160";
 
+#setup
+address="";
+things=("a" "b" "c");
+
+#init
+if [ ! -f "output.cfg" ]; then
+    address="10.10.10.160";
+else
+    address=$(head -n 1 output.cfg);
+fi
+
+#input
+echo -n "Enter ip address (empty for last: $address): " ;
+read x;
+
+if [[ $x != "" ]]; then
+    address=$x;
+fi
 
 #logic
+echo $address > output.cfg;
+
+xbapp -x $address list | grep "FrozenDistrict.HouseFlipper_5" | sed 's/ //g' >> output.cfg;
+xbapp -x $address listDLC | grep "FrozenDistrict" | sed 's/ //g' >> output.cfg;
+
 xbapp -x $address list | grep "FrozenDistrict.HouseFlipper_5"
 xbapp -x $address listDLC | grep "FrozenDistrict" 
 
-echo "Script done. . . Press enter to exit"
-read x
+#ending
+echo "Script done. . . Press enter to exit";
+read x;
